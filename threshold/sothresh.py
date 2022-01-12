60dd40889fe98443a4b409033c5bd098aa458234
Ben Taylor <46690967+benjaminztaylor@users.noreply.github.com>
2022-01-12 16:37:49 -0500

    accepts user input

--- threshold/sothresh.py
+++ threshold/sothresh.py
@@ -1,46 +1,67 @@
+import argparse
 import cv2
 import numpy as np
 
-max_block_size = 5000
-max_offset = 100
-tb_block_value = 'Block size'
-tb_offset_value = 'Offset Value'
-window_name = 'Adaptive Threshold'
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Sothresh",
+        epilog = "Adaptive Thresholding GUI",
+        add_help = "How to use",
+        prog = "python sothresh.py <arguments>")
+    parser.add_argument("-i", "--image", required = True,
+        help = "Path to input image")
+    parser.add_argument("-b", "--blocksize", default = 5000, type = int,
+        help = "Maximum blocksize [DEFAULT: 5000].")
+    parser.add_argument("-o", "--offset", default = 100, type = int,
+        help = "Maximum blocksize [DEFAULT: 100].")
+    args = vars(parser.parse_args())
 
-img_path_00 = 'test-images/17-57-59.000-resize-00050-gray.png'
-src = cv2.imread(img_path_00)
-
-# print error with invalid image path
-if src is None:
-    print(f'Input error: {img_path_00}')
-    exit(0)
+    # Collect the arguments.
+    max_block_size = args['blocksize']
+    max_offset = args['offset']
+    img_path = args['image']
     
-src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    tb_block_value = 'Block size'
+    tb_offset_value = 'Offset Value'
+    window_name = 'Adaptive Threshold'
 
-# resize imgs
-src = cv2.resize(src_gray, (640, 480))
+    src = cv2.imread(img_path)
+
+    # print error with invalid image path
+    if src is None:
+        print(f'Input error: {img_path}')
+        exit(0)
+        
+    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+    # resize imgs
+    src = cv2.resize(src_gray, (640, 480))
+        
+
+    # Values of trackbar as text on image for R trackbar.
+    font = cv2.FONT_HERSHEY_SIMPLEX
     
+    def track_adaptive_thresh(val):
 
-def track_adaptive_thresh(val):
+        offset_val = cv2.getTrackbarPos(tb_offset_value, window_name)
 
-    offset_val = cv2.getTrackbarPos(tb_offset_value, window_name)
+        block_size = cv2.getTrackbarPos(tb_block_value, window_name)
+        block_size = max(3, block_size)
+        
+        if (block_size % 2 == 0):
+                block_size  += 1
+        
+        thresh = cv2.adaptiveThreshold(src, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, block_size, offset_val)
+        thresh = cv2.putText(thresh, f"Offset:{offset_val} || Block Size:{block_size}", (10, 20), font, 0.5, (0, 0, 0), 2)
+        thresh = cv2.putText(thresh, f"Offset:{offset_val} || Block Size:{block_size}", (10, 20), font, 0.5, (255,255,255), 1)
+        cv2.imshow(window_name, thresh)
 
-    block_size = cv2.getTrackbarPos(tb_block_value, window_name)
-    block_size = max(3, block_size)
-    
-    if (block_size % 2 == 0):
-            block_size  += 1
-    
-    thresh = cv2.adaptiveThreshold(src, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, block_size, offset_val)        
-    cv2.imshow(window_name, thresh)
+    # name window
+    cv2.namedWindow(window_name)
 
-# name window
-cv2.namedWindow(window_name)
-
-# Add trackbars
-cv2.createTrackbar(tb_block_value, window_name , 0, max_block_size, track_adaptive_thresh)
-cv2.createTrackbar(tb_offset_value, window_name , 0, max_offset, track_adaptive_thresh)
+    # Add trackbars
+    cv2.createTrackbar(tb_block_value, window_name , 0, max_block_size, track_adaptive_thresh)
+    cv2.createTrackbar(tb_offset_value, window_name , 0, max_offset, track_adaptive_thresh)
 
 
-track_adaptive_thresh(3)
-cv2.waitKey()
+    track_adaptive_thresh(3)
+    cv2.waitKey()
